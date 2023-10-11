26cc32cbe751664b6d7f4b3c9205c60a9afadbd3
andreabr4 <br13.andrea@gmail.com>
2023-10-11 16:29:19 +0200

    desplegables_segundo_html

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
-    loadRegions()
-    loadSubregions()
-    loadCountries()
+   let selectCountry=document.getElementById("chooseCountry")   
+   let selectSubRegion = document.getElementById("chooseSubRegion")
+   
+   let finishRegion=document.getElementById("chooseRegion")
+   finishRegion.addEventListener("change", chosenRegion=>{
+   selectedRegion=chosenRegion.target.value
+    loadSubregions(selectedRegion)
 
-    function loadSubregions(){
+   }) 
+
+   let finishSubRegion=document.getElementById("chooseSubRegion")
+   finishSubRegion.addEventListener("change", chosenSubRegion=>{
+   selectedSubRegion=chosenSubRegion.target.value
+   loadCountries(selectedSubRegion)
+
+   }) 
+
+    function loadRegions(){
+    createPlaceholder(chooseRegion)
+    createPlaceholder(chooseSubRegion)
+    createPlaceholder(chooseCountry)
     fetch("https://restcountries.com/v3.1/all")
+    .then((response)=>(response.json()))
+    .then((data)=>{
+        let regionList=[]
+            data.forEach(countryNameRegion => {
+                let region=countryNameRegion.region
+                
+                if (!regionList.includes(region)){
+                    regionList.push(region)
+                }
+            })
+            console.log(regionList)
+
+            let selectRegion=document.getElementById("chooseRegion")
+            regionList.forEach((chooseRegion)=>{
+                
+                let createRegion= document.createElement("option")
+                createRegion.textContent=chooseRegion
+                selectRegion.appendChild(createRegion)
+            })
+    })
+    .catch((err)=>console.log(err));
+}
+
+chooseRegion = document.getElementById("chooseRegion")
+chooseRegion.addEventListener("change", regionUrl=>{
+    let selectedRegion = chooseRegion.value
+    console.log(selectedRegion)
+   removeChildren(selectCountry)
+   createPlaceholder(chooseCountry)
+})
+       
+
+
+
+    function loadSubregions(selectedRegion){
+    
+    fetch(`https://restcountries.com/v3.1/region/${selectedRegion}`)
     .then((response)=>(response.json()))
     .then((data)=>{
       
@@ -21,46 +74,35 @@
 
             console.log(subregionList)
 
+            removeChildren(selectSubRegion)
+            createPlaceholder(chooseSubRegion)
+
             subregionList.forEach((chooseSubRegion)=>{
-                    let selectSubRegion = document.getElementById("chooseSubRegion")
+                    
                     let createSubRegion= document.createElement("option")
                     if (createSubRegion== undefined){
                         console.log("error creating option ")
                     }
 
-                    createSubRegion.textContent=`${chooseSubRegion}`
+                    createSubRegion.textContent=chooseSubRegion
 
                     selectSubRegion.appendChild(createSubRegion)
             })
     })
     .catch((err)=>console.log(err));
+}
 
-}
-function loadRegions(){
-    fetch("https://restcountries.com/v3.1/all")
-    .then((response)=>(response.json()))
-    .then((data)=>{
-        let regionList=[]
-            data.forEach(countryNameRegion => {
-                let region=countryNameRegion.region
-                
-                if (!regionList.includes(region)){
-                    regionList.push(region)
-                }
-            })
-            console.log(regionList)
-            regionList.forEach((chooseRegion)=>{
-                let selectRegion=document.getElementById("chooseRegion")
-                let createRegion= document.createElement("option")
-                createRegion.textContent=`${chooseRegion}`
-                selectRegion.appendChild(createRegion)
-            })
-    })
-    .catch((err)=>console.log(err));
-}
+    chooseSubRegion = document.getElementById("chooseSubRegion");
+    chooseSubRegion.addEventListener("change", SubRegionUrl=>{
+    let selectedSubRegion = chooseSubRegion.value
+    console.log(selectedSubRegion)
+    });
+
+
    
-function loadCountries(){
-    fetch("https://restcountries.com/v3.1/all")
+function loadCountries(selectedSubRegion){
+  
+    fetch(`https://restcountries.com/v3.1/subregion/${selectedSubRegion}`)
         .then((response)=>(response.json()))
         .then((data)=>{
             let countryList=[]
@@ -71,14 +113,39 @@ function loadCountries(){
                     }
                 })
                 console.log(countryList)
+                
+
+                removeChildren(selectCountry)
+                createPlaceholder(chooseCountry)
+
                 countryList.forEach((chooseCountry)=>{
-                    let selectCountry=document.getElementById("chooseCountry")
+                    
                     let createCountry= document.createElement("option")
-                    createCountry.textContent=`${chooseCountry}`
+                    createCountry.textContent=chooseCountry
                     selectCountry.appendChild(createCountry)
-                    return countryList
                 })
         })
         .catch((err)=>console.log(err));
     }
 
+
+function removeChildren(selectOption){
+    while (selectOption.firstChild){
+    selectOption.removeChild(selectOption.firstChild)
+    }
+}
+
+
+function createPlaceholder(chosenSelection){
+    let shortenChosenSelection= chosenSelection.id.substring(6)
+    let placeholder=document.createElement("option")
+    placeholder.selected=true;
+    placeholder.disabled=true;
+    placeholder.textContent=shortenChosenSelection
+    chosenSelection.appendChild(placeholder)
+}
+
+loadRegions()
+
+
+    
